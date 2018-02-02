8d0fad0d9b7777471793d387819ebf6320a2be9f
Shanti Bethepu <shantibethepu13@gmail.com>
2018-02-02 15:28:19 -0600

    Update Subscribe.java

--- Subscribe.java
+++ Subscribe.java
@@ -1,22 +1,18 @@
 package com.ociweb.aws;
-
 import com.ociweb.gl.api.GreenCommandChannel;
 import com.ociweb.gl.api.GreenRuntime;
 import com.ociweb.gl.api.PubSubMethodListener;
 import com.ociweb.gl.api.StartupListener;
 import com.ociweb.pronghorn.pipe.ChannelReader;
-
-import java.util.Scanner;
-
-public class Subscribe implements StartupListener,PubSubMethodListener {
-
+public class Subscribe implements StartupListener,PubSubMethodListener 
+{
     GreenCommandChannel cmd;
     String subcribetopic;
-    public Subscribe(GreenRuntime runtime, String topic) {
+    public Subscribe(GreenRuntime runtime, String topic)
+    {
         cmd = runtime.newCommandChannel(DYNAMIC_MESSAGING);
         this.subcribetopic = topic;
     }
-
     @Override
     public void startup() {
         System.out.println("\nSubscribing to the topic "+subcribetopic);
